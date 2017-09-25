7d2dd8c419825317caf3f4c095fe0402554e7344
Hulien <hulien.hugo@gmail.com>
2017-09-25 15:51:17 +0200

    Ajout du loop pour lire les posts

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php get_header();?>
 <section><h3>partie centrale</h3></section>
 <h3> Sous titre</h3>
+<!-- The Loop -->
+<?php if (have_posts()):while (have_posts()):the_post();?>
+<h3><?php the_title();?></h3>
+<p><?php the_content();?></p>
+<?php endwhile;endif;?>
+<!-- Fin Loop -->
 <?php get_footer();?>
