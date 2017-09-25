26bf875cf6be9e4dc401cf17b9a7159c4b105b80
Hulien <hulien.hugo@gmail.com>
2017-09-25 14:58:17 +0200

    1ère version du thème. Rien a voir, y'a que les bases.

---
+++ header.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <title><?php bloginfo('name');?></title>
+    <?php wp_head();?>
+    <header>
+      <h1><?php bloginfo('name');?></h1>
+      <h1><?php bloginfo('description');?></h1>
+    </header>
+  </head>
+  <body>
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php include('header.php');?>
+<section><h3>partie centrale</h3></section>
+<?php include('footer.php');?>
